c3c7ca1ef9b16bc9820e8160f56d7918233d5669
M <m@cognusion.com>
2025-04-25 18:01:22 -0400

    Removes old lexicon-builder.

--- lex_test.go
+++ lex_test.go
@@ -9,17 +9,6 @@ var (
 	testSize = 6
 )
 
-func Benchmark_NewLexiconOld(b *testing.B) {
-	LexHook = func(s *Phrase) bool {
-		return false
-	}
-
-	verbose = false
-	for i := 0; i < b.N; i++ {
-		_ = newLexiconFromFileOld(testFile, testSize)
-	}
-}
-
 func Benchmark_NewLexicon(b *testing.B) {
 	LexHook = func(s *Phrase) bool {
 		return false
@@ -28,18 +17,6 @@ func Benchmark_NewLexicon(b *testing.B) {
 	verbose = false
 	for i := 0; i < b.N; i++ {
 		_ = NewLexiconFromFile(testFile, testSize)
-	}
-}
-
-func Benchmark_NewLexiconOldVerbose(b *testing.B) {
-	LexHook = func(s *Phrase) bool {
-		return false
-	}
-
-	verbose = true
-
-	for i := 0; i < b.N; i++ {
-		_ = newLexiconFromFileOld(testFile, testSize)
 	}
 }
 
